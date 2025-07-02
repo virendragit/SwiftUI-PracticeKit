80ecf4d30b0bac19d5e90cf7c629d1cf17704d96
virendra <virendra.gupta@alliantgroup.com>
2025-07-02 18:30:50 +0530

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by Virendra Gupta on 02/07/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

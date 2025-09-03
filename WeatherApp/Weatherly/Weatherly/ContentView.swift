d9199c25590f1db9372b867cdb508c06d767270c
virendra <virendra.gupta@alliantgroup.com>
2025-09-03 18:56:02 +0530

    Added new prject for weather app setup

---
+++ WeatherApp/Weatherly/Weatherly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weatherly
+//
+//  Created by Virendra Gupta on 03/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherApp/Weatherly/Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by Virendra Gupta on 03/09/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

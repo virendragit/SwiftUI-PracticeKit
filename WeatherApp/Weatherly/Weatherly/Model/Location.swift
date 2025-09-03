4ece26556e53d80588d58a7428a3b20bb6169ac6
virendra <virendra.gupta@alliantgroup.com>
2025-09-03 19:09:13 +0530

    added location model

---
+++ WeatherApp/Weatherly/Weatherly/Model/Location.swift
@@ -0,0 +1,14 @@
+//
+//  Location.swift
+//  Weatherly
+//
+//  Created by Virendra Gupta on 03/09/25.
+//
+
+import Foundation
+
+struct Location  : Decodable{
+    let name: String
+    let lat: Double
+    let lon: Double
+}

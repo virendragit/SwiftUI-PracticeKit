830e16eca602c7334c6253b603e5a0583434d986
Virendra Gupta <virendragupta7@gmail.com>
2025-09-08 12:19:14 +0530

    added geocoding client

---
+++ WeatherApp/Weatherly/Weatherly/Clients/GeocodingClient.swift
@@ -0,0 +1,35 @@
+//
+//  GeocodingClient.swift
+//  Weatherly
+//
+//  Created by Virendra Gupta on 08/09/25.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case noData
+    case decodingError
+    case invalidURL
+    case badURLResponse
+}
+
+struct GeocodingClient{
+    func coordinateByCity(_ city: String) async throws -> Location? {
+     
+//        let (data ,response) = try await URLSession.shared.data(from: URL(string: "https://api.opencagedata.com/geocode/v1/json?q=\(city)&key=6b311b59-f15b-4511-a45e-e5c1645e466e")!)
+        
+        let (data, response) = try await URLSession.shared.data(from: APIEndpoint.endpointURL(for: .coordinatesByLocation(city)))
+        
+        guard let httpResponse = response as? HTTPURLResponse ,
+            httpResponse.statusCode == 200 else {
+            throw NetworkError.invalidURL
+            }
+        
+        let locations = try JSONDecoder().decode([Location].self, from: data)
+        return locations.first
+        
+        }
+        
+    }
+

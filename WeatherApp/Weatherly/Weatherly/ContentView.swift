9b25e27249f5af769008407410a8c46bbd60ad2e
Virendra Gupta <virendragupta7@gmail.com>
2025-09-08 12:39:06 +0530

    Added call for geoCoding client

--- WeatherApp/Weatherly/Weatherly/ContentView.swift
+++ WeatherApp/Weatherly/Weatherly/ContentView.swift
@@ -10,12 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Weather APp")
+            Button("Get Coordinates"){
+                Task {
+                    let geoCodingClient = GeocodingClient()
+                    let location = try! await geoCodingClient.coordinateByCity("New York")
+                    print("\(location)")
+                    
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 

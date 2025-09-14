4bab42ab36c377b300c211324b2c56ac509139a2
Virendra Gupta <virendragupta7@gmail.com>
2025-09-14 11:44:37 +0530

    MVVM Example app

---
+++ MVVMSample/MVVMSample/MVVMSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSampleApp.swift
+//  MVVMSample
+//
+//  Created by Virendra Gupta on 14/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

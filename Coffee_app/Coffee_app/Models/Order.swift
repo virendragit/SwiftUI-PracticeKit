1ae432be215cf97e95321173e948f408694035a6
Virendra Gupta <virendragupta7@gmail.com>
2025-09-15 07:55:31 +0530

    init changes

--- Coffee_app/Coffee_app/Models/Order.swift
+++ Coffee_app/Coffee_app/Models/Order.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+enum CoffeeSize: String, Codable ,CaseIterable {
+    case small = "Small"
+    case medium = "Medium"
+    case large = "Large"
+}
+
+
+
+struct Order: Codable, Identifiable , Hashable {
+    
+    var id: Int?
+    var name: String
+    var coffeeName: String
+    var total: Double
+    var size:CoffeeSize
+    
+}
--- Coffee_app/Coffee_app/Services/Webservice.swift
+++ Coffee_app/Coffee_app/Services/Webservice.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+enum NetworkError: Error {
+    case badURL
+    case badRequest
+    case noData
+    case unknown
+}
+
+class Webservice{
+    
+    func getOrders() async throws -> [Order] {
+        
+        //https://island-bramble.glitch.me/test/orders
+        
+        guard let url = URL(string: "https://island-bramble.glitch.me/test/orders") else {
+           throw NetworkError.badURL
+        }
+        
+        let (data, response) =  try await URLSession.shared.data(from: url)
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw NetworkError.badRequest
+        }
+        
+        guard let orders = try? JSONDecoder().decode([Order].self, from: data) else {
+            throw NetworkError.unknown
+        }
+        
+        return orders
+    }
+}

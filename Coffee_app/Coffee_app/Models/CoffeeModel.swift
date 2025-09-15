794fe733da6d775ec26b7badf482faa53f1d2e6f
Virendra Gupta <virendragupta7@gmail.com>
2025-09-15 08:02:54 +0530

    added

--- Coffee_app/Coffee_app/Models/CoffeeModel.swift
+++ Coffee_app/Coffee_app/Models/CoffeeModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+class CoffeeModel: ObservableObject {
+    
+    let webservices : Webservice
+    @Published private(set) var coffees: [Order] = []
+    
+    init(webservices: Webservice) {
+        self.webservices = webservices
+    }
+    
+    func populateOrders() async throws{
+        
+    }
+}

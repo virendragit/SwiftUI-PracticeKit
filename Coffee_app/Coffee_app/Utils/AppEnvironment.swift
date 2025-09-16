da299c27b8ff056c150cd7affb119897da50453b
Virendra Gupta <virendragupta7@gmail.com>
2025-09-16 23:05:13 +0530

    App env

--- Coffee_app/Coffee_app/Utils/AppEnvironment.swift
+++ Coffee_app/Coffee_app/Utils/AppEnvironment.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+enum EndPoits{
+    case allorders
+    
+    var path: String {
+        switch self {
+        case .allorders:
+            return "/orders"
+        }
+    }
+}
+
+enum AppEnvironment: String {
+    case development
+    case production
+    
+    
+    var baseURL: URL {
+        switch self {
+        case .development:
+            return URL(string: "https://jsonplaceholder.typicode.com")!
+        case .production:
+            fatalError("Production URL not yet defined")
+        }
+    }
+}
